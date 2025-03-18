8b2e20621942ef4a48c2ed91a9646296ed6d60a2
Camila Villarreal <villarreal.cam23@gmail.com>
2025-03-18 17:04:40 -0600

    inicializando repositorio

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-/*const inputAmigo = document.getElementById("amigo");
-const listaAmigos = [];
-const ulListaAmigos = document.getElementById("listaAmigos");
-const ilResultado = document.getElementById("resultado");
 
-function agregarAmigo() {
-    listaAmigos.push(inputAmigo.value); 
-    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
-};
-
-function sortearAmigo() {
-    const random = Math.floor(Math.random() * listaAmigos.length);
-    const amigoSecreto = listaAmigos[random];
-    ulResultado.innerHTML = `<li> El amigo secreto es: ${amigoSecreto}</li>`;
-};
-*/
 let amigos = [];
 
 /**
